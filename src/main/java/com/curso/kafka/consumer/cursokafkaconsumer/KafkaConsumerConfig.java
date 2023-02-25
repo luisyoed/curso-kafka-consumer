d88cb6a0309b71303100571751b4211a0ecf587e
Guillermo Rojas <memo.onilink@gmail.com>
2023-02-25 15:12:11 -0600

    sesion2

--- src/main/java/com/curso/kafka/consumer/cursokafkaconsumer/KafkaConsumerConfig.java
+++ src/main/java/com/curso/kafka/consumer/cursokafkaconsumer/KafkaConsumerConfig.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -15,20 +17,56 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 @EnableKafka
 @Configuration
 public class KafkaConsumerConfig {
-	@Bean
-	public ConsumerFactory<String, String> consumerFactory() {
-		Map<String, Object> props =  new HashMap<>();
-		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+	static final Logger log = LoggerFactory.getLogger(KafkaConsumerConfig.class);
+
+//	@Bean
+//	public ConsumerFactory<String, String> consumerFactory() {
+//		Map<String, Object> props = new HashMap<>();
+//		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092, localhost:9093, localhost:9094");
+//		props.put(ConsumerConfig.GROUP_ID_CONFIG, "test-consumer-group");
+//		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+//		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+//		props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+//		props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+//
+//		return new DefaultKafkaConsumerFactory<>(props);
+//	}
+//
+//	@Bean
+//	public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+//		ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+//		factory.setConsumerFactory(consumerFactory());
+//		return factory;
+//	}
+
+
+	
+	
+	
+	 @Bean
+	public ConsumerFactory<String, String> consumerFactory(String groupId) {
+		Map<String, Object> props = new HashMap<>();
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092, localhost:9093, localhost:9094");
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
 		props.put(ConsumerConfig.GROUP_ID_CONFIG, "test-consumer-group");
 		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-		return new  DefaultKafkaConsumerFactory<>(props);
+		props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+		props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+
+		return new DefaultKafkaConsumerFactory<>(props);
 	}
-	
+
 	@Bean
-	public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(){
-		ConcurrentKafkaListenerContainerFactory<String, String> factory =  new ConcurrentKafkaListenerContainerFactory<>();
-		factory.setConsumerFactory(consumerFactory());
+	public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(String groupId) {
+		ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+		factory.setConsumerFactory(consumerFactory(groupId));
 		return factory;
 	}
+
+	@Bean
+	public ConcurrentKafkaListenerContainerFactory<String, String> partitionsKafkaListenerContainerFactory() {
+		return kafkaListenerContainerFactory("partitions");
+	}
+	 
 }
--- src/main/java/com/curso/kafka/consumer/cursokafkaconsumer/serviceImpl/Consumer.java
+++ src/main/java/com/curso/kafka/consumer/cursokafkaconsumer/serviceImpl/Consumer.java
@@ -3,14 +3,32 @@ package com.curso.kafka.consumer.cursokafkaconsumer.serviceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.kafka.annotation.PartitionOffset;
 
 @Service
 public class Consumer {
-	static final Logger  log = LoggerFactory.getLogger(Consumer.class);
-	
-	@KafkaListener(topics = "curso-kafka-test",  groupId = "test-consumer-group")
-	public void listenGroup(String message) {
-		log.info("Mensaje en el grupo test-consumer-group {}", message);
+	static final Logger log = LoggerFactory.getLogger(Consumer.class);
+
+//	@KafkaListener(topics = "curso-kafka-test-3", groupId = "test-consumer-group")
+//	public void listenGroup(String message) {
+//		log.info("Mensaje en el grupo test-consumer-group {} ", message);
+//	}
+
+//	@KafkaListener(topics = "curso-kafka-test-3", groupId = "test-consumer-group")
+//	public void listenWithHeaders(@Payload String message, @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
+//		log.info("Received Message: {}, from partition:{} ", message, partition);
+//	}
+
+	@KafkaListener(topicPartitions = @TopicPartition(topic = "curso-kafka-test-3" , partitionOffsets = {
+			@PartitionOffset(partition = "0", initialOffset = "0"),
+			@PartitionOffset(partition = "1", initialOffset = "0"),
+			@PartitionOffset(partition = "2", initialOffset = "0") }), groupId = "cursokafka3", containerFactory = "partitionsKafkaListenerContainerFactory")
+	public void listenToPartition(@Payload String message, @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
+		log.info("Received Message: {}, from partition:{} ", message, partition);
 	}
 }
